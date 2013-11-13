63d8d7abca9f8cf22e90f7b18c40990a6e595805
Rikard Holm <rikardholm@gmail.com>
2013-11-13 23:15:27 +0100

    Removed unused modules

--- simple-rest-forms-javascript/src/test/javascript/HelloerSpec.js
+++
@@ -1,9 +0,0 @@
-describe('Helloer', function() {
-    it('should be able to say hello', function() {
-        var helloer = new Helloer();
-
-        var message = helloer.sayHello();
-
-        expect(message).toEqual('Hello!');
-    });
-});
--- simple-rest-forms-taglib/src/main/resources/META-INF/tags/input.tag
+++
@@ -1,5 +0,0 @@
-<%@ attribute name="name" required="true" type="java.lang.String" %>
-<label for="${name}">
-${name}
-<input type="text" name="${name}"/>
-</label>

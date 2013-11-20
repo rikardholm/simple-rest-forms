ee326a2062d16be1be96bd30ae386a3ca811cbf8
Rikard Holm <rikardholm@gmail.com>
2013-11-20 23:28:04 +0100

    Setup Requirejs build

---
+++ simple-rest-forms-composite/src/build/rjs/config.js
@@ -0,0 +1,6 @@
+({
+    name: 'simple-rest-forms/SimpleForm',
+    out: '../../../target/rjs-work/SimpleForm-built.js',
+    baseUrl: '../../main/webapp/WEB-INF/javascript',
+    paths: {jquery: 'empty:'}
+})
